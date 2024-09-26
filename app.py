a8467336262f333b6c0a32bccd6ec2fe1add0e9e
Rizwan Khan <rk331319@gmail.com>
2024-09-26 18:11:03 -0500

    Routing for Flask app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import torch
+from receiptPrediction import loadModel
+
+app = Flask(__name__)
+
+model = loadModel()
+#routing for web
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    month = data.get('month', None)
+
+    if month is None:
+        return jsonify({'error': 'Please select a month'}), 400
+    
+    input = torch.tensor([[month]], dtype=torch.float32)
+    with torch.no_grad():
+        prediction = model(input).item
+
+    return jsonify({'Predicted number of receipts': prediction})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
