Debug and fix low predictions with normalization

app.py
@@ -4,7 +4,7 @@ from receiptPrediction import loadModel
 
 app = Flask(__name__)
 
-model = loadModel()
+model, maxCount = loadModel()
 #routing for web
 @app.route('/')
 def home():
@@ -19,9 +19,9 @@ def predict():
     # Prepare input for the model
     input_tensor = torch.tensor([[month]], dtype=torch.float32)
     with torch.no_grad():
-        prediction = model(input_tensor).item()
-
-    return jsonify({'Predicted number of receipts': prediction})
+        normalPrediction = model(input_tensor).item()
+        prediction = normalPrediction * maxCount
+    return jsonify({'predicted_receipts': prediction})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)

receiptPrediction.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import pandas as pd
 import numpy as np
+import json
 
 def preprocessing(file):
     data = pd.read_csv(file)
@@ -20,16 +21,21 @@ class receiptPrediction(nn.Module):
     #input is month, output is count
     def __init__(self):
         super(receiptPrediction, self).__init__()
-        self.fc1 = nn.Linear(1, 8)
-        self.fc2 = nn.Linear(8, 1)
+        self.fc1 = nn.Linear(1, 16)
+        self.fc2 = nn.Linear(16, 8)
+        self.fc3 = nn.Linear(8, 1)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
     
 #Training
 def train(monthlyData):
+    maxCount = monthlyData['Receipt_Count'].max()
+    monthlyData['Receipt_Count'] /= maxCount
+
     X = torch.tensor(monthlyData['Month'].values, dtype=torch.float32).view(-1,1)
     y = torch.tensor(monthlyData['Receipt_Count'].values, dtype=torch.float32).view(-1,1)
     #Initialize model, loss, and optim
@@ -38,7 +44,7 @@ def train(monthlyData):
     optimizer = optim.Adam(model.parameters(), lr=0.01)
 
     #training iterations
-    for epoch in range(50):
+    for epoch in range(200):
         model.train()
         optimizer.zero_grad()
         outputs = model(X)
@@ -48,6 +54,9 @@ def train(monthlyData):
     
     #save model
     torch.save(model.state_dict(), 'model.pth')
+    #save maxCount
+    with open('config.json', 'w') as config_file:
+        json.dump({"maxCount": maxCount}, config_file)
     return model
 
 #load model
@@ -55,12 +64,17 @@ def loadModel():
     model = receiptPrediction()
     model.load_state_dict(torch.load('model.pth'))
     model.eval()
-    return model
+    #grab maxCount
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+        maxCount = config["maxCount"]
+    return model, maxCount
 
 #training routine
 def main():
     monthlyData = preprocessing('data_daily.csv')
-    train(monthlyData)
+    print(monthlyData)
+    model = train(monthlyData)
     print('Training complete')
 
 if __name__ == '__main__':