07eeaa542d98688b4470dd81c070c0d52327fc8e
Rizwan Khan <rk331319@gmail.com>
2024-09-26 17:39:24 -0500

    Define NN model

--- receiptPrediction.py
+++ receiptPrediction.py
@@ -4,3 +4,9 @@ import torch.optim as optim
 import numpy as np
 
 class receiptPrediction(nn.Module):
+    def __init__(self):
+        super(receiptPrediciton, self).__init__()
+        self.fc1 = nn.Linear(1, 16)
+        self.fc2 = nn.Linear(16, 8)
+        self.fc3 = nn.Linear(8, 1)
+
